fb7e88622f56d16432fe66621d751064b7c09634
Andy Pierce <andy@andy-VirtualBox.(none)>
2013-05-29 09:11:18 -0700

    Finished user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name                  "Andy Pierce"
+    email                 "redjet736@gmail.com"
+    password              "andyandy"
+    password_confirmation "andyandy"
+  end
+end
